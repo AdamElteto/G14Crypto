3bf50baa943f5d13b7d20fe38947b9b37e96abf5
Adam Elteto <github@elteto.com>
2016-07-01 23:31:56 -0700

    Update onetimepad.sh

--- onetimepad.sh
+++ onetimepad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#This script generates a 1024-letter one-time pad.
+#This script generates a 1025-letter one-time pad.
 #The output is to the screen.
-#The size of the pad can be changed by adjusting "1024".
-< /dev/random tr -dc A-Z | head -c${1:-1024};echo;
+#The size of the pad can be changed by adjusting "1025".
+#The letters are arranged in groups of five
+#on lines of five groups each for easier reading.
+</dev/random tr -dc A-Z | head -c${1:-1025} | fold -w5 | paste -sd' ' | fold -w30;echo;
